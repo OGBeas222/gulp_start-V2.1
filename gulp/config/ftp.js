dae422e89909dc9a1d438acf4ce5911adf970c80
OGBeas222 <zholudev111@gmail.com>
2022-01-09 16:43:16 +0300

    first commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "d990923l.beget.tech",
+    user: "d990923l_veatem",
+    password: "PwLHd7*D",
+    parallel: 5
+}
---
+++ gulp/tasks/server.js
@@ -0,0 +1,11 @@
+
+
+export const server = (done) => {
+    app.plugins.browserSync.init({
+        server: {
+            baseDir: `${app.path.build.html}`
+        },
+        notify: true,
+        port: 3000,
+    });
+}
